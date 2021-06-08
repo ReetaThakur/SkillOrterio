c6ba148fb12e6d9ae003efdaaa9c679c5ba07006
AppStudio <om@appstudiointernal.ca>
2021-06-08 16:47:45 +0530

    # Quiz WIP

---
+++ app/src/main/java/com/app/skillontario/quiz/QuizAc.java
@@ -0,0 +1,61 @@
+package com.app.skillontario.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import android.os.Bundle;
+
+import com.app.skillontario.adapter.TabAdapter;
+import com.app.skillontario.baseClasses.BaseActivity;
+import com.app.skillorterio.R;
+import com.app.skillorterio.databinding.ActivityHelpBinding;
+import com.app.skillorterio.databinding.QuizAcBinding;
+
+public class QuizAc extends BaseActivity {
+    private TabAdapter tabAdapter;
+    private QuizAcBinding binding;
+
+    @Override
+    protected void initUi() {
+        overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_from_left);
+        binding = (QuizAcBinding) viewBaseBinding;
+
+        setAdapter();
+
+    }
+
+
+    private void setAdapter() {
+        tabAdapter = new TabAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        tabAdapter.addFragment(new QuizChildFragment("1"), "");
+        tabAdapter.addFragment(new QuizChildFragment("2"), "");
+        tabAdapter.addFragment(new QuizChildFragment("3"), "");
+        tabAdapter.addFragment(new QuizChildFragment("4"), "");
+        // tabAdapter.addFragment(new GetStartedFr("5"), "");
+        // tabAdapter.addFragment(new GetStartedFr("6"), "");
+
+
+        binding.viewPager.setAdapter(tabAdapter);
+       // binding.tab.setupWithViewPager(binding.viewPager, true);
+
+       /*
+        binding.viewPager.setPagingEnabled(false);
+        binding.viewPager.setOffscreenPageLimit(4);*/
+
+
+    }
+
+
+
+    @Override
+    protected int getLayoutById() {
+        return R.layout.quiz_ac;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_from_right);
+
+    }
+}
