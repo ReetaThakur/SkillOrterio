6284f086c12e91bc31b7262627f9bfb8c8f31826
AppStudio <om@appstudiointernal.ca>
2021-06-09 21:07:30 +0530

    Release Note:
    Quiz Scree 11.2
    Select Question Quiz 11.3
    Select other option type Quiz Screen With
    Completed Quiz Screen 11.5
    Select Quiz Screen 11.6
    Congratulations! with multiple users
    Share Quiz
    -------------
    Invite Cell : WIP
    Quiz Final Selection at count 10: WIP

---
+++ app/src/main/java/com/app/skillontario/adapter/QuizStepAdapter.java
@@ -0,0 +1,125 @@
+package com.app.skillontario.adapter;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.media.MediaController2;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.app.skillorterio.R;
+import com.app.skillorterio.databinding.AdapterNotificationBinding;
+import com.app.skillorterio.databinding.QuizStepItemABinding;
+import com.app.skillorterio.databinding.QuizStepItemBBinding;
+
+import org.jetbrains.annotations.NotNull;
+
+public class QuizStepAdapter extends RecyclerView.Adapter<QuizStepAdapter.ViewHolder> {
+    Context context;
+int count;
+    public QuizStepAdapter(Context context,int count) {
+        this.context = context;
+        this.count=count;
+    }
+    String hold="";
+    String[] qus=new String[]{
+            "Excited/Fulfilling",
+            "Challenging/Intriguing",
+            "Anxious/Draining",
+            "Indifferent/Neutral"};
+    String[] opt=new String[]{
+            "A","B","C","D"};
+
+    String[] optB=new String[]{
+            "Excited",
+            "Challenging",
+            "Anxious",
+            "Indifferent"
+    };
+
+    int[] optBimg=new int[]{R.drawable.o_a,R.drawable.o_b,R.drawable.o_c,R.drawable.o_d};
+
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 4;
+    }
+
+    @Override
+    public void onBindViewHolder(final QuizStepAdapter.ViewHolder viewHolder, final int position) {
+
+        if(count==4){
+            viewHolder.bindingb.ivOption.setImageResource(optBimg[position]);
+            viewHolder.bindingb.tvTitle.setText(optB[position]);
+            if (hold.equals(String.valueOf(position))) {
+                viewHolder.bindingb.row.setImageResource(R.drawable.ic_selected_click);
+             }
+            viewHolder.bindingb.row.setOnClickListener(view -> {
+                if (hold.equals(""))
+                    hold = String.valueOf(position);
+                notifyDataSetChanged();
+            });
+        }else {
+            viewHolder.binding.tvOption.setText(opt[position]);
+            viewHolder.binding.tvTitle.setText(qus[position]);
+            if (hold.equals(String.valueOf(position))) {
+                viewHolder.binding.row.getBackground().setColorFilter(Color.parseColor("#34C759"), PorterDuff.Mode.SRC_ATOP);
+                viewHolder.binding.ivOption.setColorFilter(ContextCompat.getColor(context, R.color.select), android.graphics.PorterDuff.Mode.MULTIPLY);
+                viewHolder.binding.tvOption.setTextColor(context.getResources().getColor(R.color.white));
+                viewHolder.binding.tvTitle.setTextColor(context.getResources().getColor(R.color.white));
+            }
+            viewHolder.binding.row.setOnClickListener(view -> {
+                if (hold.equals(""))
+                    hold = String.valueOf(position);
+                notifyDataSetChanged();
+            });
+        }
+
+    }
+
+    @NotNull
+    @Override
+    public QuizStepAdapter.ViewHolder onCreateViewHolder(@NotNull ViewGroup parent, int position) {
+        if (count==4) {
+            QuizStepItemBBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                    R.layout.quiz_step_item_b, parent, false);
+            return new QuizStepAdapter.ViewHolder(binding);
+        }else {
+            QuizStepItemABinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                    R.layout.quiz_step_item_a, parent, false);
+            return new QuizStepAdapter.ViewHolder(binding);
+        }
+    }
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private QuizStepItemABinding binding;
+        public ViewHolder(QuizStepItemABinding view) {
+            super(view.getRoot());
+            binding = view;
+        }
+
+        private QuizStepItemBBinding bindingb;
+        public ViewHolder(QuizStepItemBBinding view) {
+            super(view.getRoot());
+            bindingb = view;
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+
+
+}
--- app/src/main/java/com/app/skillontario/quiz/TakeQuizAc.java
+++ app/src/main/java/com/app/skillontario/quiz/TakeQuizAc.java
@@ -1,21 +1,29 @@
 package com.app.skillontario.quiz;
+import android.content.Intent;
+
+import com.app.skillontario.SignIn.WelcomeActivity;
 import com.app.skillontario.adapter.TabAdapter;
 import com.app.skillontario.baseClasses.BaseActivity;
 import com.app.skillorterio.R;
 import com.app.skillorterio.databinding.TakeQuizAcBinding;
 
 public class TakeQuizAc extends BaseActivity {
-    private TabAdapter tabAdapter;
     private TakeQuizAcBinding binding;
 
     @Override
     protected void initUi() {
         overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_from_left);
         binding = (TakeQuizAcBinding) viewBaseBinding;
+        binding.actionBarL.tvTitle.setText(getResources().getText(R.string.take_qz_ttl));
 
+        binding.done.setOnClickListener(v->startActivity(new Intent(this, QuizStepAc.class)));
+       noDataSet();
+    }
+
+    private void noDataSet() {
         binding.noData.ivNoData.setImageResource(R.drawable.ic_take_quiz);
         binding.noData.tvNoData.setText(getResources().getText(R.string.take_qz));
-
+        binding.noData.tvNoData.setTextColor(getResources().getColor(R.color.black));
     }
 
     @Override
