1a6e9f285c2a977e0e08256a6fd4f5e0c3f899d2
AppStudio <debi@appstudiointernal.ca>
2021-06-09 19:43:05 +0530

    first code

---
+++ app/src/main/java/com/app/skillontario/activities/PartnersActivity.java
@@ -0,0 +1,47 @@
+package com.app.skillontario.activities;
+
+import com.app.skillontario.adapter.NotificationAdapter;
+import com.app.skillontario.baseClasses.BaseActivity;
+import com.app.skillontario.utils.RecyclerItemClickListener;
+import com.app.skillorterio.R;
+import com.app.skillorterio.databinding.ActivityNotificationBinding;
+
+public class PartnersActivity extends BaseActivity {
+
+    private ActivityNotificationBinding binding;
+    private NotificationAdapter adapter;
+
+    @Override
+    protected void initUi() {
+        overridePendingTransition(R.anim.slide_in_from_right, R.anim.slide_out_from_left);
+        binding = (ActivityNotificationBinding) viewBaseBinding;
+
+        binding.actionBar.tvTitle.setText("Notifications");
+        showNotificationRecycler();
+
+        binding.actionBar.ivBack.setOnClickListener(v -> onBackPressed());
+
+    }
+
+    private void showNotificationRecycler() {
+        binding.recyNotification.setHasFixedSize(true);
+        adapter = new NotificationAdapter(PartnersActivity.this);
+        binding.recyNotification.setAdapter(adapter);
+
+        binding.recyNotification.addOnItemTouchListener(new RecyclerItemClickListener(PartnersActivity.this, (view, position) -> {
+
+        }));
+    }
+
+    @Override
+    protected int getLayoutById() {
+        return R.layout.activity_notification;
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        overridePendingTransition(R.anim.slide_in_from_left, R.anim.slide_out_from_right);
+
+    }
+}
